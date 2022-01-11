8abe6024081eb8997fa2e501e35d1c2f23bf3b76
tahelz <tahel28@gmail.com>
2022-01-11 17:11:18 +0200

    workflow

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,38 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const http = __importStar(require("http"));
+const cors_1 = __importDefault(require("cors"));
+const dotenv = __importStar(require("dotenv"));
+const routes_1 = require("./routes");
 const app = (0, express_1.default)();
-const port = 3000;
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
-app.listen(port, () => {
-    return console.log(`Express is listening at http://localhost:${port}`);
-});
+const server = http.createServer(app);
+dotenv.config();
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use((0, cors_1.default)());
+app.use(routes_1.router);
+server.listen(process.env.PORT, () => console.log(`Server listening on port ${process.env.PORT}`));
 //# sourceMappingURL=app.js.map
